fe096465cc0d9b3f31f2bf2a9de79bf55cc1e709
partitan <partitan@gmail.com>
2016-11-24 14:35:08 +0700

    Create twtProxy.php

---
+++ twtProxy.php
@@ -0,0 +1,18 @@
+<?php
+//Twitter API
+echo "Twitter API test";
+
+//Twitter APP
+$consumerKey    = '4MTtBPsekfyeS1gGLIndqbWbC';
+$consumerSecret = 'WGBrMsZacLjCp3KPGhxyvDHske0N9YS1ZZiewnaGOXVpfst04J';
+$oAuthToken     = '747679427291676673-vuIY9VA4WL1e0t0kBkqL5P4uF3S4GHZ';
+$oAuthSecret    = 'NUqaIFUzx5iiPkMsqlEvzhJe3hrO1JXxLQ2mwvYX36gph';
+
+require_once('twitteroauth.php');
+$tweet = new TwitterOAuth($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecret);
+
+
+
+
+
+//Send Message
