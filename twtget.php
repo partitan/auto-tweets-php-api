9bc5e5b40e92aed3d81738367a21417bb2953f06
partitan <partitan@gmail.com>
2016-11-26 01:41:38 +0700

    Update twtget.php

--- twtget.php
+++ twtget.php
@@ -19,8 +19,8 @@ $tweet = new TwitterOAuth($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecr
 
 $cxpReply = $tweet-> get('direct_messages', array('count' => 1,'skip_status' => 1, 'include_entities' => false));
 	$a = json_decode($cxpReply, true);
-echo $a;
-//print_r($a)."<br>";
+//echo $a;
+print_r($a);
 //print_r(array_keys($a));
 //print_r(array_keys($a,"0");
 echo "------";// output 8
