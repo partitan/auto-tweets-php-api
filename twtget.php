9eb03f0c27aa7df052f8e9df16f43eeaeb3eefba
partitan <partitan@gmail.com>
2016-11-26 01:09:41 +0700

    Update twtget.php

--- twtget.php
+++ twtget.php
@@ -14,7 +14,7 @@ $tweet = new TwitterOAuth($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecr
 
 
 //echo $tweet->get('followers/ids', array('screen_name' => 'prungkrae', 'count'=> 5000));
-echo $tweet-> get('direct_messages', array('count' => 1));
+echo $tweet-> get('direct_messages', array('count' => 1),array('include_entities') => false);
 
 
 //$cxpReply = $tweet-> get('direct_messages', array('count' => 1));
